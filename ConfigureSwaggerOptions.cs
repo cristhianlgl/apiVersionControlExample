71945e1a47bc1f7557279470a56e6bf027f91e3a
cristhianlgl <cristhianlgl@hotmail.com>
2022-09-28 08:34:35 -0500

    Agregar configuraciones en Program.cs para solicitudes Http y configuraciones de versionamiento en el proyecto y en swagger

--- ConfigureSwaggerOptions.cs
+++ ConfigureSwaggerOptions.cs
@@ -44,7 +44,7 @@ namespace ApiVersionControl
 
         public void Configure(string name, SwaggerGenOptions options)
         {
-            throw new NotImplementedException();
+            Configure(options);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 using ApiVersionControl.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using ApiVersionControl;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // add api test settings
 builder.Services.Configure<ApiTestSettings>(builder.Configuration.GetSection("ApiTest"));
+
+//1. Add httpClient to sent HttpRequets in controllers
+builder.Services.AddHttpClient();
+
+//2. Add app Versioning Control
+builder.Services.AddApiVersioning(setup =>
+{
+    setup.DefaultApiVersion = new ApiVersion(1,0);
+    setup.AssumeDefaultVersionWhenUnspecified = true;
+    setup.ReportApiVersions = true;
+});
+
+//3 add configuration to document versions 
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+
 
 // Add services to the container.
 
@@ -13,13 +35,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//4. Configure options
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//5. Configure Endpoints for swagger DOCS for each of versions of our api
+var apiVersionDescriptioProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+//6. Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptioProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint(
+                $"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant()
+            );
+        }
+    });
 }
 
 app.UseHttpsRedirection();
